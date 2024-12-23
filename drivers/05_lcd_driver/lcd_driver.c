e280195c9b0556fdbcffb661679d76dece9ba1e0
Lucian Ari <lucian.ariseanu@yahoo.com>
2024-12-23 22:33:43 +0200

    Add lcd driver

--- drivers/05_lcd_driver/lcd_driver.c
+++ drivers/05_lcd_driver/lcd_driver.c
@@ -4,11 +4,12 @@
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
 #include <linux/gpio.h>
+#include <linux/delay.h>
 
 /* Meta Information */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("NA");
-MODULE_DESCRIPTION("A simple gpio driver for setting a LED and reading a button");
+MODULE_DESCRIPTION("A simple driver to write to a LCD text display");
 
 
 /* Variables for device and device class */
@@ -16,29 +17,65 @@ static dev_t my_device_nr;
 static struct class *my_class;
 static struct cdev my_device;
 
-#define DRIVER_NAME  "my_gpio_driver"
+#define DRIVER_NAME  "lcd"
 #define DRIVER_CLASS "MyModuleClass"
-/** 
- * @brief Read data out of the buffer
+
+/* LCD char buffer */
+static char lcd_buffer[17];
+
+/* Pinout for LCD Display */
+unsigned int gpios[] = {
+    3,  /* Enable pin */
+    2,  /* Register select pin */
+    4,  /* Data Pin 0 */
+    17, /* Data Pin 1 */
+    27, /* Data Pin 2 */
+    22, /* Data Pin 3 */
+    10, /* Data Pin 4 */
+    9,  /* Data Pin 5 */
+    11, /* Data Pin 6 */
+    5,  /* Data Pin 7 */  
+};
+
+#define REGISTER_SELECT gpios[1]
+
+/**
+ * @brief generates a pulse on the enable signal
  */
-static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
-    int to_copy, not_copied, delta;
-    char tmp[3] = " \n";
+void lcd_enable(void){
+    gpio_set_value(gpios[0], 1);
+    msleep(5);
+    gpio_set_value(gpios[0], 0);
+}
 
-    /* Get amount of data to copy */
-    to_copy = min(count, sizeof(tmp));
+/**
+ * @brief set the 8 bit data bus
+ * @param data: Data to set  
+ */
+void lcd_send_byte(char data) {
+    int i;
+    for(i=0; i<8; i++)
+        gpio_set_value(gpios[i+2], ((data) & (1<<i)) >> i);
+    lcd_enable();
+    msleep(5);
+}
 
-    /* Read value of button */
-    printk("Value of button: %d\n", gpio_get_value(17));
-    tmp[0] = gpio_get_value(17) + '0';
+/**
+ * @brief send a command to the LCD
+ * @param data: command to send
+ */
+void lcd_command(uint8_t data) {
+    gpio_set_value(REGISTER_SELECT, 0);  /*RS to Instruction*/
+    lcd_send_byte(data);
+}
 
-    /* Copy data to user */
-    not_copied = copy_to_user(user_buffer, &tmp, to_copy);
-
-    /* Calculate data */
-    delta = to_copy - not_copied;
-
-    return delta;
+/**
+ * @brief send data to the :CD
+ * @param data: command to send
+ */
+void lcd_data(uint8_t data) {
+    gpio_set_value(REGISTER_SELECT, 1); /*RS to data*/
+    lcd_send_byte(data);
 }
 
 /**
@@ -49,25 +86,19 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
     char value;
 
     /* Get amount of data to copy */
-    to_copy = min(count, sizeof(value));
+    to_copy = min(count, sizeof(lcd_buffer));
 
     /* Copy data from user */
-    not_copied = copy_from_user(&value, user_buffer, to_copy);
+    not_copied = copy_from_user(lcd_buffer, user_buffer, to_copy);
  
-    /* Setting the LED */
-    switch(value){
-        case '0':
-            gpio_set_value(4,0);
-            break;
-        case '1':
-            gpio_set_value(4,1);
-            break;
-        default:
-            printk("Invalid Input!\n");
-            break;
-    }
     /* Calculate data */
     delta = to_copy - not_copied;
+
+    /* Set the new data to the display */
+    lcd_command(0x1);
+
+    for(int i=0; i<to_copy;i++)
+        lcd_data(lcd_buffer[i]);
 
     return delta;
 }
@@ -92,15 +123,15 @@ static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = driver_open,
     .release = driver_close,
-    .read = driver_read,
     .write = driver_write
 };
 /**
  * @brief This function is called, when the module is loaded into the kernel 
  */
 static int __init ModuleInit(void) {
-    int retval;	
-    printk("Hello, Kernel\n");
+    int i;
+    char *names[] = {"ENABLE_PIN", "REGISTER_SELECT", "DATA_PIN0", "DATA_PIN1", "DATA_PIN2", "DATA_PIN3", "DATA_PIN4", "DATA_PIN5", "DATA_PIN6", "DATA_PIN7"}; 
+    printk("Hello, Kernel!\n");
 
     /* Allocate a device nr */
     if(alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
@@ -129,36 +160,38 @@ static int __init ModuleInit(void) {
         goto AddError;
     }
 
-    /* GPIO 4 init */
-    if(gpio_request(4, "rpi-gpio-4")) {
-        printk("Can not allocate GPIO 4\n");
-        goto AddError;
+    /* Initialize GPIOs */
+    printk("lcd-driver - GPIO Init\n");
+    for(i=0; i<10; i++){
+        if(gpio_request(gpios[i], names[i])){
+            printk("lcd-driver - Error Init GPIO %d \n", gpios[i]);
+            goto GpioInitError;
+        }
     }
-    
-    /* Set GPIO 4 direction */
-    if(gpio_direction_output(4, 0)) {
-        printk("Can not sert GPIO 4 to output\n");
-        goto Gpio4Error;
+    printk("lcd-driver - GPIO Init\n");
+    for(i=0; i<10; i++){
+        if(gpio_direction_output(gpios[i], 0)){
+            printk("lcd-driver - Error setting GPIO %d to output\n", i);
+            goto GpioDirectionError;
+        }
     }
 
-    /* GPIO 17 init */
-    if(gpio_request(17, "rpi-gpio-17")) {
-        printk("Can not allocate GPIO 17\n");
-        goto Gpio4Error;
-    }
-    
-    /* Set GPIO 17 direction */
-    if(gpio_direction_input(17)) {
-        printk("Can not sert GPIO 17 to input\n");
-        goto Gpio17Error;
-    }
+    /* Init the dispay */
+    lcd_command(0x30); /* Set the display for 8 bit data interface */
+    lcd_command(0xf);  /* Turn display on, turn cursor on, set cursor blinking */
+    lcd_command(0x1);
+
+    char text[] = "Hello world!";
+    for(i=0; i <sizeof(text)-1;i++)
+        lcd_data(text[i]);
 
     return 0;
 
-Gpio17Error:
-    gpio_free(17);
-Gpio4Error:
-    gpio_free(4);
+GpioDirectionError:
+    i=9;
+GpioInitError:
+    for(;i>=0;i--)
+        gpio_free(gpios[i]);
 AddError:
     device_destroy(my_class, my_device_nr);
 FileError:
@@ -172,9 +205,12 @@ ClassError:
  * @brief This function is called, when the module is removed from the kernel 
  */
 static void __exit ModuleExit(void) {
-    gpio_set_value(4,0);
-    gpio_free(17);
-    gpio_free(4);
+    int i;
+    lcd_command(0x1);   /* Clear the display */
+    for(i=0; i<10; i++){
+        gpio_set_value(gpios[i],0);
+        gpio_free(gpios[i]);
+    }
     cdev_del(&my_device);
     device_destroy(my_class, my_device_nr);
     class_destroy(my_class);
