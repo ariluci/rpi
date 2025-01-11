ef9654e77bd4b53e6459595cc8b1ff97e24fc5f3
lucian.ariseanu <lucian.ariseanu@yahoo.com>
2025-01-11 23:21:11 +0200

    Bugfix for 07_bmp280

--- drivers/07_bmp280/bmp280.c
+++ drivers/07_bmp280/bmp280.c
@@ -1,3 +1,5 @@
+/* !!! To read temperature use sudo cat /dev/bm280 or sudo head -n 1 dev/bmp280 !!!*/
+
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
@@ -11,24 +13,22 @@
 #define DRIVER_NAME "bmp280"
 #define DRIVER_CLASS "bmp280Class"
 
-static struct i2c_adapter *bmp_i2c_adapter = NULL;  /* Adapter from driver to I2C Bus */
-static struct i2c_client *bmp280_i2c_client = NULL; /* ClientL Our I2c device */
+static struct i2c_adapter * bmp_i2c_adapter = NULL;
+static struct i2c_client * bmp280_i2c_client = NULL;
 
-/* Meta information */
-MODULE_AUTHOR("NA");
+/* Meta Information */
+MODULE_AUTHOR("Johannes 4Linux");
 MODULE_LICENSE("GPL");
-
 MODULE_DESCRIPTION("A driver for reading out a BMP280 temperature sensor");
-MODULE_SUPPORTED_DEVICE("NONE");
 
-/* Defines for device identification */
-#define I2C_BUS_AVAILABLE		1  			/* The I2C available on the raspberry */
-#define SLAVE_DEVICE_NAME		"BMP280"	/* Device and Driver Name */
-#define BMP280_SLAVE_ADDRESS	0x76		/* BMP280 I2C adddress */
+/* Defines for device identification */ 
+#define I2C_BUS_AVAILABLE	1		/* The I2C Bus available on the raspberry */
+#define SLAVE_DEVICE_NAME	"BMP280"	/* Device and Driver Name */
+#define BMP280_SLAVE_ADDRESS	0x76		/* BMP280 I2C address */
 
 static const struct i2c_device_id bmp_id[] = {
-	{ SLAVE_DEVICE_NAME, 0 },
-	{ }
+		{ SLAVE_DEVICE_NAME, 0 }, 
+		{ }
 };
 
 static struct i2c_driver bmp_driver = {
@@ -42,7 +42,7 @@ static struct i2c_board_info bmp_i2c_board_info = {
 	I2C_BOARD_INFO(SLAVE_DEVICE_NAME, BMP280_SLAVE_ADDRESS)
 };
 
-/* Variables for device and device class */
+/* Variables for Device and Deviceclass*/
 static dev_t myDeviceNr;
 static struct class *myClass;
 static struct cdev myDevice;
@@ -53,12 +53,12 @@ s32 dig_T1, dig_T2, dig_T3;
 /**
  * @brief Read current temperature from BMP280 sensor
  *
- * return temperature in degree
+ * @return temperature in degree
  */
- s32 read_temperature(void) {
+s32 read_temperature(void) {
 	int var1, var2;
 	s32 raw_temp;
-	s32 d1,d2,d3;
+	s32 d1, d2, d3;
 
 	/* Read Temperature */
 	d1 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFA);
@@ -66,7 +66,7 @@ s32 dig_T1, dig_T2, dig_T3;
 	d3 = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xFC);
 	raw_temp = ((d1<<16) | (d2<<8) | d3) >> 4;
 
-	/* Calculate temperature */
+	/* Calculate temperature in degree */
 	var1 = ((((raw_temp >> 3) - (dig_T1 << 1))) * (dig_T2)) >> 11;
 
 	var2 = (((((raw_temp >> 4) - (dig_T1)) * ((raw_temp >> 4) - (dig_T1))) >> 12) * (dig_T3)) >> 14;
@@ -74,31 +74,31 @@ s32 dig_T1, dig_T2, dig_T3;
 }
 
 /**
- * @brief Read data out of the buffer
+ * @brief Get data out of buffer
  */
 static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
 	int to_copy, not_copied, delta;
 	char out_string[20];
 	int temperature;
-	
-	/* Get amount of data to copy */
-	to_copy = min(sizeof(out_string),count);
-	
+
+	/* Get amount of bytes to copy */
+	to_copy = min(sizeof(out_string), count);
+
 	/* Get temperature */
 	temperature = read_temperature();
 	snprintf(out_string, sizeof(out_string), "%d.%d\n", temperature/100, temperature%100);
-	
-	/* Copy data to user space */
+
+	/* Copy Data to user */
 	not_copied = copy_to_user(user_buffer, out_string, to_copy);
 
-	/* Calculate data */
+	/* Calculate delta */
 	delta = to_copy - not_copied;
 
 	return delta;
 }
 
 /**
- * @brief This function is called when the deice file is opened
+ * @brief This function is called, when the device file is opened
  */
 static int driver_open(struct inode *deviceFile, struct file *instance) {
 	printk("MyDeviceDriver -  Open was called\n");
@@ -106,90 +106,94 @@ static int driver_open(struct inode *deviceFile, struct file *instance) {
 }
 
 /**
- * @brief This function is called when the device file is closed
+ * @brief This function is called, when the device file is close
  */
 static int driver_close(struct inode *deviceFile, struct file *instance) {
-    printk("MyDeviceDriver -  Close was called\n");
-    return 0;
+	printk("MyDeviceDriver -  Close was called\n");
+	return 0;
 }
 
+/* Map the file operations */
 static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = driver_open,
-    .release = driver_close,
-    .read = driver_read,
+	.owner = THIS_MODULE,
+	.open = driver_open,
+	.release = driver_close,
+	.read = driver_read,
 };
+
+
 /**
- * @brief This function is called when the module is loaded into the kernel
+ * @brief function, which is called after loading module to kernel, do initialization there
  */
 static int __init ModuleInit(void) {
-    int ret = -1;	
+	int ret = -1;
 	u8 id;
-    printk("MyDeviceDriver - Hello Kernel\n");
+	printk("MyDeviceDriver - Hello Kernel\n");
 
-    /* Allocate a device nr */
-    if(alloc_chrdev_region(&myDeviceNr, 0, 1, DRIVER_NAME) < 0) {
-        printk("Device Nr. could not be allocated!\n");
-    }
-    printk("MyDeviceDriver - Device Nr %d was registered\n", myDeviceNr);
-    
-    /* Create device class */
-    if((myClass = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
-        printk("Device Class can not be created!\n");
-        goto ClassError;
-    }
-    /* Create device file */
-    if(device_create(myClass, NULL, myDeviceNr, NULL, DRIVER_NAME) == NULL){
-        printk("Can not create device file!\n");
-        goto FileError;
-    }
+	/* Allocate Device Nr */
+	if ( alloc_chrdev_region(&myDeviceNr, 0, 1, DRIVER_NAME) < 0) {
+		printk("Device Nr. could not be allocated!\n");
+	}
+	printk("MyDeviceDriver - Device Nr %d was registered\n", myDeviceNr);
 
-	/* Initialize device file */
+	/* Create Device Class */
+	if ((myClass = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+		printk("Device Class can not be created!\n");
+		goto ClassError;
+	}
+
+	/* Create Device file */
+	if (device_create(myClass, NULL, myDeviceNr, NULL, DRIVER_NAME) == NULL) {
+		printk("Can not create device file!\n");
+		goto FileError;
+	}
+
+	/* Initialize Device file */
 	cdev_init(&myDevice, &fops);
 
-	/* Register device to kernel */
-	if(cdev_add(&myDevice, myDeviceNr, 1) == -1) {
+	/* register device to kernel */
+	if (cdev_add(&myDevice, myDeviceNr, 1) == -1) {
 		printk("Registering of device to kernel failed!\n");
 		goto KernelError;
 	}
 
-	bmp_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE); /* adapter to get access to the bus*/
+	bmp_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE);
 
 	if(bmp_i2c_adapter != NULL) {
-		bmp280_i2c_client = i2c_acpi_new_device(bmp_i2c_adapter, &bmp_i2c_board_info);
-			if(bmp280_i2c_client != NULL) {
-				if(i2c_add_driver(&bmp_driver) != -1) {
-					ret = 0;
-				}
-				else 
-					printk("Can't add driver...\n");
+		bmp280_i2c_client = i2c_new_client_device(bmp_i2c_adapter, &bmp_i2c_board_info);
+		if(bmp280_i2c_client != NULL) {
+			if(i2c_add_driver(&bmp_driver) != -1) {
+				ret = 0;
 			}
-			i2c_put_adapter(bmp_i2c_adapter);
+			else
+				printk("Can't add driver...\n");
+		}
+		i2c_put_adapter(bmp_i2c_adapter);
 	}
 	printk("BMP280 Driver added!\n");
 
-	/* Read ID */
-	id = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xD0);	
+	/* Read Chip ID */
+	id = i2c_smbus_read_byte_data(bmp280_i2c_client, 0xD0);
 	printk("ID: 0x%x\n", id);
-	
-	/* Read callibartion variavles */
+
+	/* Read Calibration Values */
 	dig_T1 = i2c_smbus_read_word_data(bmp280_i2c_client, 0x88);
 	dig_T2 = i2c_smbus_read_word_data(bmp280_i2c_client, 0x8a);
 	dig_T3 = i2c_smbus_read_word_data(bmp280_i2c_client, 0x8c);
 
 	if(dig_T2 > 32767)
 		dig_T2 -= 65536;
-		
+
 	if(dig_T3 > 32767)
 		dig_T3 -= 65536;
-	/* Initialize device */
+
+	/* Initialice the sensor */
 	i2c_smbus_write_byte_data(bmp280_i2c_client, 0xf5, 5<<5);
 	i2c_smbus_write_byte_data(bmp280_i2c_client, 0xf4, ((5<<5) | (5<<2) | (3<<0)));
-
 	return ret;
 
 KernelError:
-    device_destroy(myClass, myDeviceNr);
+	device_destroy(myClass, myDeviceNr);
 FileError:
 	class_destroy(myClass);
 ClassError:
@@ -198,42 +202,18 @@ ClassError:
 }
 
 /**
- * @brief This function is called when the module is removed from the kernel
+ * @brief function, which is called when removing module from kernel
  * free alocated resources
  */
 static void __exit ModuleExit(void) {
-		printk("MyDeviceDriver - Goodbye, Kernel!\n");
-		i2c_unregister_device(bmp280_i2c_client);
-		i2c_del_driver(&bmp_driver);
-		cdev_del(&myDevice);
-	device_destroy(myClass, myDeviceNr);
-	class_destroy(myClass);
-	unregister_chrdev_region(myDeviceNr, 1);
-
+	printk("MyDeviceDriver - Goodbye, Kernel!\n");
+	i2c_unregister_device(bmp280_i2c_client);
+	i2c_del_driver(&bmp_driver);
+	cdev_del(&myDevice);
+    device_destroy(myClass, myDeviceNr);
+    class_destroy(myClass);
+    unregister_chrdev_region(myDeviceNr, 1);
 }
 
 module_init(ModuleInit);
 module_exit(ModuleExit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
